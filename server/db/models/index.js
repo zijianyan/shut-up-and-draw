d17e6f2e2246af1450b91993ff36fc2f1aa283db
Emily Farro <emily.farro@gmail.com>
2018-11-18 22:33:09 -0500

    rough sequelize models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,70 @@
 const User = require('./user')
+const db = require('../db')
+
+const Game = db.define('game', {
+  roundNumber: {
+    type: db.Sequelize.INTEGER
+  },
+  players: {
+    type: db.Sequelize.ARRAY(db.Sequelize.STRING)
+  },
+  // submissions: {
+  //   type: db.Sequelize.ARRAY(db.Sequelize.STRING)
+  // },
+})
+
+const Submission = db.define('entry', {
+  type: {
+    type: db.Sequelize.ENUM('drawing', 'guess')
+  },
+  guess: {
+    type: db.Sequelize.STRING
+  },
+  drawingUrl: {
+    type: db.Sequelize.STRING
+  },
+})
+
+
+Submission.belongsTo(Game)
+Submission.belongsTo(User)
+
+
+// const syncAndSeed = async ()=> {
+//   try {
+//     await db.sync({force: true})
+//     const [Emily, Cang, Zi] = await Promise.all([
+//         User.create({ name: 'emily'}),
+//         User.create({ name: 'cang' }),
+//         User.create({ name: 'zi' }),
+//     ])
+//     const [Game1] = await Promise.all([
+//       Game.create({ roundNumber: 1}),
+//     ])
+
+
+//     const [answer1] = await Promise.all([
+//       Submission.create({ guess: 'dog'}),
+//     ])
+//     const [response1, response2] = await Promise.all([
+//       Submission.create({ type: 'guess', guess: 'horse'}),
+//       Submission.create({ type: 'drawing', drawingUrl: 'img.jpeg'}),
+//     ])
+
+
+//     await Promise.all([
+//       answer1.setGame(Game1),
+//       answer1.setUser(Zi),
+//       response1.setUser(Cang),
+//       response2.setUser(Emily),
+//     ])
+//   } catch(error) {
+//     console.log(error)
+//   }
+
+// };
+
+
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -14,5 +80,7 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  Game,
+  Submission
 }
