7b644da3d8ac82914601746d8b1233258cb45494
Zijian Yan <zi@zijianyan.com>
2018-11-20 15:44:34 -0500

    reverted user properties to original, removed name property

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,9 +3,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
-  name: {
-    type: Sequelize.STRING,
-  },
   email: {
     type: Sequelize.STRING,
     unique: true,
