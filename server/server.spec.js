d801774007bfb4558bdb7fdeb45b16f133a73ac3
Zijian Yan <zi@zijianyan.com>
2018-11-20 00:10:23 -0500

    removed console.logs

--- server/server.spec.js
+++ server/server.spec.js
@@ -42,7 +42,6 @@ describe('Playing the game', ()=> {
         expect(createdGame).to.be.ok;
         expect(createdGame.submissions.length).to.equal(1);
         expect(createdGame.roundNumber).to.equal(0);
-        // console.log('createdGame:', createdGame.dataValues);
 
         return app.post(`/api/submissions/${createdGame.id}`)
           .send({ type: 'drawing', drawingUrl: 'placeholder-url', gameId: createdGame.id, userId: zi.id }) // a drawing submission
@@ -58,26 +57,11 @@ describe('Playing the game', ()=> {
               ]
             });
 
-            // db.Page.findAll({
-            //   include: [{
-            //     model: db.Gallery
-            //     include: [{
-            //       model: db.Artwork
-            //     }]
-            //   }],
-            //   order: [
-            //     [ db.Gallery, 'order', 'DESC' ],
-            //     [ db.Gallery, db.ArtWork, 'order', 'DESC' ]
-            //   ]
-            // })
-            
-
             expect(createdGame.roundNumber).to.equal(1);
             expect(createdGame.submissions.length).to.equal(2);
-
             expect(createdGame.submissions[1].type).to.equal('drawing');
             
-            // return app.post(
+            
           })
           
 
@@ -85,66 +69,4 @@ describe('Playing the game', ()=> {
   });
 });
 
-
-
-// router.post('/gameId', async (req, res, next) => {
-//   // POST a submission in association to the game it's being created within
-//   // needs to come to this route with these attributes in the body
-//   try {
-//     const { type } = req.body;
-//     let submission;
-
-//     if(type === 'phrase') {
-//       submission = await Submission.create({
-//         type: req.body.type,
-//         phrase: req.body.phrase,
-//         gameId: req.params.gameId
-//       });
-//     } else {
-//       // this is TBD because we haven't set up AWS and
-//       // I'm unsure where the drawing URL is coming from
-//       submission = await Submission.create({
-//         type: req.body.type,
-//         drawingUrl: req.body.drawing,
-//         gameId: req.params.gameId
-//       });
-//     }
-
-//     // after submission is created, we find the game and update the roundNumber pointer
-//     const game = await Game.findById(req.params.gameId)
-//     game.roundNumber++
-//     await game.save()
-
-//     res.send(submission);
-//   } catch(err) {
-//     next(err)
-//   }
-// })
-
-
-
-// const Game = db.define('game', {
-//   roundNumber: {
-//     type: db.Sequelize.INTEGER,
-//     defaultValue: 0
-//   },
-//   players: {
-//     type: db.Sequelize.ARRAY(db.Sequelize.STRING)
-//   },
-//   status: {
-//     type: db.Sequelize.ENUM('active', 'complete')
-//   }
-// })
-
-// const Submission = db.define('submission', {
-//   type: {
-//     type: db.Sequelize.ENUM('drawing', 'phrase')
-//   },
-//   phrase: {
-//     type: db.Sequelize.STRING
-//   },
-//   drawingUrl: {
-//     type: db.Sequelize.STRING
-//   },
-// })
 
