ae1c5af7a44aa83c1208a2a503163acd93970a29
Zijian Yan <zi@zijianyan.com>
2018-11-19 23:14:31 -0500

    fixed games typo

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,7 @@ module.exports = router
 
 router.use('/users', require('./users'))
 
-router.use('/game', require('./games') )
+router.use('/games', require('./games') )
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
