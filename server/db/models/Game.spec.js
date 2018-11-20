ff0d35b67d183b9a07dccb638536345e30b7bd16
Cang Truong <cangtruong1@gmail.com>
2018-11-19 20:43:23 -0500

    testing game

---
+++ server/db/models/Game.spec.js
@@ -0,0 +1,30 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const db = require('../index')
+const Game = db.model('game')
+
+describe('Game model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('instanceMethods', () => {
+    describe('correctPassword', () => {
+      let game
+
+      beforeEach(async () => {
+        game = await Game.create({})
+      })
+
+      it('adds', ()=> {
+        expect(1+1).to.equal(2)
+      })
+      it('game exists', ()=> {
+        expect(game.id).to.be.ok
+      })
+
+
+    }) // end describe('correctPassword')
+  }) // end describe('instanceMethods')
+}) // end describe('User model')
