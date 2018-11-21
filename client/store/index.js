7983478543291f9345a58dfc1eccd4e6e03a66fe
Zijian Yan <zi@zijianyan.com>
2018-11-20 21:24:58 -0500

    imported users reducer into combineReducers

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,12 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import users from './users'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({
+  user,
+  users
+})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
