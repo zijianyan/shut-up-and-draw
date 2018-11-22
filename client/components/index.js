308b884c98e23b2ac5add37ff8fc29a200e4344f
Emily Farro <emily.farro@gmail.com>
2018-11-22 12:51:32 -0500

    saving facebook passport oauth

--- client/components/index.js
+++ client/components/index.js
@@ -5,4 +5,5 @@
  */
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
+export {default as PlayerList} from './player-list'
 export {Login, Signup} from './auth-form'
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, PlayerList} from './components'
 import {me} from './store'
 
 /**
@@ -25,6 +25,8 @@ class Routes extends Component {
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
+            <Route path="/selectplayers" component={PlayerList} />
+
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
