4d3932ac6dd7c744d6b20c8d0a135a3a87549c02
Emily Farro <emily.farro@gmail.com>
2018-11-24 22:42:25 -0500

    retry CI with environment vars

--- server/twilio.js
+++ server/twilio.js
@@ -2,9 +2,8 @@
 const twilio = require('twilio');
 
 
-const twilioClient = twilio('AC9d3c93251038f476201a6ff834b0765e', process.env.TWILIO_AUTH);
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
 
-// verify numbers twilio.com/user/account/phone-numbers/verified
 const nudgeText = phoneNumber => {
   twilioClient.messages
     .create({
