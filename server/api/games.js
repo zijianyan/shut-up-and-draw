9940deb834f185a9615b05d6dfe3b414c28455a3
Zijian Yan <zi@zijianyan.com>
2018-11-25 20:15:39 -0500

    added sequelize hook for game model to switch status to complete, commented out same logic from routes, trying to account for nudge after last submission

--- server/api/games.js
+++ server/api/games.js
@@ -153,20 +153,22 @@ router.post('/:id/submissions', isLoggedIn, async (req, res, next) => {
 
     // after submission is created, we find the game and update the roundNumber pointer
     const game = await Game.findById(gameId)
-    if(game.roundNumber+1 >= game.players.length) {
-      game.status = 'complete'
-    } else {
-      game.roundNumber = game.roundNumber+1
-    }
+    // if(game.roundNumber+1 >= game.players.length) {
+    //   game.status = 'complete'
+    // } else {
+    //   game.roundNumber = game.roundNumber+1
+    // }
+    game.roundNumber = game.roundNumber+1
 
-    User.findById(game.players[game.roundNumber])
+    User.findById(game.players[game.roundNumber]) 
     .then(user => {
-      nudgeText(user.phoneNumber)
-      console.log('nudged the user', user.name)
+      if (user) { // if it's the last submission, there won't be a user found
+        nudgeText(user.phoneNumber)
+        console.log('nudged the user', user.name)
+      }
     })
 
     await game.save()
-
 
     res.send(submission);
   } catch(err) {
